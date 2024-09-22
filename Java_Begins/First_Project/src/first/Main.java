01b4534cd2e9f2b9206390fb484fe569ea6514ab
mathabethakore <syed.tahmid15@gmail.com>
2024-09-23 00:43:18 +0600

    1st upload

---
+++ Java_Begins/First_Project/src/first/Main.java
@@ -0,0 +1,12 @@
+package first;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ("I love coding (as long as it is productive and not a nerdy shitshow)");
+		System.out.println("hi");
+		System.out.println();
+	}
+
+}
