7bbd45b59509b1b9a4e97feea5fe0c3844434a1b
mathabethakore <syed.tahmid15@gmail.com>
2024-09-23 02:13:58 +0600

    added variables and inputs lesson

---
+++ Java_Begins/First_Project/src/first/Taking_Input.java
@@ -0,0 +1,21 @@
+package first;
+
+import java.util.Scanner;
+public class Taking_Input {
+	public static void main(String[]args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("What is your name?: ");
+		String name = scanner.nextLine();
+		System.out.println("Hello there "+name+"! Welcome to OOP hell");
+		System.out.println("Enter Age: ");
+		int value = scanner.nextInt();
+		scanner.nextLine();//clear next line char because next int does not clear it
+		System.out.println("Emter your favourite game: ");
+		String game = scanner.nextLine();
+		System.out.println("You are "+ value+ " years old!\nYour favourite game is: "+ game);
+		
+		
+				}
+}
